b84c795a5fc6c34bd343c156a15f712965cf6524
Michal Nedzka <michael.nedzka@gmail.com>
2019-08-25 12:08:03 +0200

    First tests

--- src/example.js
+++ src/example.js
@@ -1,10 +1,30 @@
 import fs from 'fs'
 
-export function multiply (a, b) {
+export function multiply(a, b) {
   return a * b
 }
 
-export function countLines (fileName) {
-  const content = fs.readFileSync(fileName, 'utf-8')
+
+export function countLines_parameterSeam(fileName, doReadFile = readFile) {
+  const content = doReadFile(fileName)
   return content.split('\n').length
+}
+
+function readFile(fileName) {
+  return fs.readFileSync(fileName, 'utf-8');
+}
+
+
+
+export function countLines_globalSeam(fileName) {
+  const content = readFile_global(fileName)
+  return content.split('\n').length
+}
+
+function readFile_global(fileName) {
+  if (process.env.NODE_ENV === 'test') {
+    global.fileName = fileName
+    return global.fileContent
+  }
+  return fs.readFileSync(fileName, 'utf-8');
 }
---
+++ test/example.test.js
@@ -0,0 +1,50 @@
+import { multiply, countLines_globalSeam, countLines_parameterSeam } from '../src/example'
+import { expect } from 'chai'
+
+describe('multiply', () => {
+  it('multiplies two numbers', () => {
+    // GIVEN
+    let a = 5
+    let b = 4
+
+    // WHEN
+    let result = multiply(a, b)
+
+    // THEN
+    expect(result).to.equal(20)
+  })
+})
+
+describe('countLines', () => {
+  it('#countLines_globalSeam returns 3 for 3 line file', () => {
+    // GIVEN
+    global.fileContent = 'a\nb\nc'
+    const fileName = 'file.txt'
+
+    // WHEN
+    const result = countLines_globalSeam(fileName)
+
+    // THEN
+    expect(global.fileName).to.equal(fileName)
+    expect(result).to.equal(3)
+  })
+
+  it('#countLines_parameterSeam returns 3 for 3 line file', () => {
+    // GIVEN
+    const fileContent = 'a\nb\nc'
+    const fileName = 'file.txt'
+    let param
+
+    function readFile(fileName) {
+      param = fileName
+      return fileContent
+    }
+
+    // WHEN
+    const result = countLines_parameterSeam(fileName, readFile)
+
+    // THEN
+    expect(param).to.equal(fileName)
+    expect(result).to.equal(3)
+  })
+})
